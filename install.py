e34030ef7abab523388109060e86748b370682c4
ZSShen <andy.zsshen@gmail.com>
2020-04-22 17:40:19 -0700

    Experiment: Test GCP docker-compose - part 16.

--- install.py
+++ install.py
@@ -5,8 +5,9 @@ import requests
 
 def main():
     samples = os.listdir(sys.argv[1])
-    index = 0
+    #index = 0
 
+    payloads = []
     for sample in samples:
         if not sample.endswith(".json"):
             continue
@@ -15,15 +16,19 @@ def main():
         if not os.path.isfile(sample_path):
             continue
 
-        url = f"""http://localhost:9200/cheetsheet/python/{index}"""
-
         payload = None
         with open(sample_path, "r") as hdle:
             payload = hdle.read()
+        payloads.append(payload)
 
-        result = requests.post(url, data=payload)
-        print(f"""Install {sample_path} ...""")
-        index += 1
+    len_payloads = len(payloads)
+    bound = 100000
+    for id in range(0, bound):
+        idx = id % len_payloads
+        url = f"""http://localhost:9200/cheetsheet/python/{id}"""
+        result = requests.post(url, json=payloads[idx])
+        #print(f"""Install {sample_path} ...""")
+        #index += 1
 
 
 if __name__ == "__main__":
