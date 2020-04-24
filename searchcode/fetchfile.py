9a3e75b24d20055570b3f1c275af83268b2c879f
aalexx-S <alex530519@gmail.com>
2020-04-23 18:39:16 -0700

    Finish searcode implementation.

---
+++ searchcode/fetchfile.py
@@ -0,0 +1,87 @@
+import os
+import requests
+
+from .searchcodeapicaller import SearchcodeApiCaller
+
+def work(outputdir, base_url, *, start=0, offset=1, per_page=20, num_limit=0):
+    """
+    Call searchcode api starting from page={start} and with {offset} pages.
+    Stop when result is empty.
+    Only doenload files from github.
+    Download files from master branch, then try develop branch if master branch doesn't exist.
+
+    Downloaded files will be named as {username}_{reponame}_{path_to_file}_{filename}.py
+    Files with the same name will be wiped out.
+
+    Arguments:
+        outputdir:
+            A directory to put all the output file
+        base_url:
+            Searchcode api url.
+
+    Keyword Arguments:
+        start:
+            Starting page. Start from 0.
+        offset:
+            Offset pages. p=start, p=start + offset ...
+        per_page:
+            Number of repository per page (per request).
+        num_limit:
+            Numer of repository to download accross all threads. Set 0 for no limit.
+    """
+    BRANCH = ['master', 'develop']
+
+    github_raw = 'https://raw.githubusercontent.com/'
+
+    caller = SearchcodeApiCaller()
+    page = start - offset
+
+    while True:
+        page += offset
+
+        # return when reach download limit
+        if num_limit > 0 and  page * per_page > num_limit:
+            return
+
+        url = f'{base_url}&per_page={per_page}&p={page}'
+        res = caller.call(url)
+
+        if res is None or res['results'] is None or not res['results']:
+            return
+
+        # max number
+        result_repos = res['results']
+        if num_limit > 0:
+            result_repos = result_repos[:max(min(len(res), num_limit - page * per_page), 0)]
+
+        for result in result_repos:
+            ## form url to get target file.
+            # parse username
+            username = result['repo'].find('github.com/')
+            username = result['repo'][username + 11:]
+            username = username[:username.find('/')]
+
+            # try branches in BRANCH in order
+            success = False
+            for branch in BRANCH:
+                # form url
+                file_url = f'{github_raw}{username}/{result["name"]}/{branch}'
+                if result['location']:
+                    file_url = f'{file_url}{result["location"]}'
+                file_url = f'{file_url}/{result["filename"]}'
+
+                # download file and store it
+                file_res = requests.get(file_url)
+                if file_res.ok:
+                    # filename
+                    ofn = os.path.join(outputdir, f'{username}_{result["name"]}_{result["location"].replace(os.path.sep, "_")}_{result["filename"]}')
+                    try:
+                        with open(ofn, 'bw') as of:
+                            of.write(file_res.content)
+                    except Exception as e:
+                        print(e)
+                    success = True
+                    break
+
+            if not success:
+                print(f'File not found: repo={result["repo"]}, loc={result["location"]}, file={result["filename"]}')
---
+++ searchcode/searchcode.py
@@ -0,0 +1,46 @@
+import argparse
+import os
+import threading
+
+from .searchcodeapicaller import SearchcodeApiCaller
+from .fetchfile import work
+
+def main(myarg):
+    # form query url for searchcode api
+    # src: 2 = from github
+    # lan: 19 = python
+    base_url = f'https://searchcode.com/api/codesearch_I/?q={"+".join(myarg.Q)}&src=2&lan=19'
+
+    if myarg.num == -1:
+        # just print number of results
+        # call api
+        res = SearchcodeApiCaller().call(base_url)
+        print(f'Number of results: {res["total"]}')
+        exit(0)
+
+    # create output dir if not exist
+    os.makedirs(myarg.output, exist_ok=True)
+
+    # create workers pool and start working
+    worker_pool = [None for _ in range(myarg.thread)]
+    for i in range(myarg.thread):
+        worker_pool[i] = threading.Thread(target=work, args=(myarg.output, base_url),
+                kwargs={'start': i, 'offset': myarg.thread, 'per_page': myarg.per_query, 'num_limit': myarg.num})
+        worker_pool[i].start()
+
+    # join all and exit
+    for i in range(myarg.thread):
+        worker_pool[i].join()
+
+
+if __name__ == '__main__':
+    # parse arg
+    parser = argparse.ArgumentParser(description='Clone git repos with keywords')
+    parser.add_argument('-n', '--num', type=int, default=-1, help='Number of repo to download, pass 0 for all. Only print out number of repo by default.')
+    parser.add_argument('-t', '--thread', type=int, default=1, help='Number of threads. It make sense since most workload should be on disk writing.')
+    parser.add_argument('-p', '--per_query', type=int, default=20, help='Max number of result per query.')
+    parser.add_argument('-o', '--output', type=str, default=os.getcwd(), help="A director to store all downloaded files. The directory will be created if it doesn't exist")
+    parser.add_argument('Q', type=str, nargs='+', help="Keywords to query. Don't need to type 'Q', just type keywords.")
+    arg = parser.parse_args()
+
+    main(arg)
